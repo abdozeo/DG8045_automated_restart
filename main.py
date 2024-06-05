9039dc18733a90f0c175303d24507f62e3f1f728
Abd0 <abdulrahmanshaat5@gmail.com>
2024-06-05 09:01:12 +0300

    add Log-in process

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(options=options)
+
+USR = "admin"
+PASS = "ADD_YOUR_GATEWAY_PASSWORD_HERE"
+
+driver.get("http://192.168.1.1")
+username = WebDriverWait(driver,5).until(EC.presence_of_element_located((By.ID,"index_username")))
+password = driver.find_element(By.ID,"password")
+username.send_keys(USR)
+password.send_keys(PASS + Keys.ENTER)
+time.sleep(3) #Succesful LogIn
